a491247b288460fe700e16c3ba6feabe8928de71
Michael Cruz <mrcruz1989@gmail.com>
2023-10-26 12:25:05 -0400

    sending menu data

--- drf-test/menu/views.py
+++ drf-test/menu/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.http import JsonResponse
+
+from .models import Item
 
 # Create your views here.
+
+
+def item_list(request):
+    items = Item.objects.all()
+    item_list = []
+    for item in items:
+        item_list.append({
+            'name': item.name,
+            'price': item.price,
+            'description': item.description
+        })
+    return JsonResponse({"menu_items": item_list})
